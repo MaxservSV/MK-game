5755381f5bc8a2b94946f36838755428af0fa8fb
MaxservSV <78587688+MaxservSV@users.noreply.github.com>
2021-06-04 15:21:45 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,81 @@
+const $arenas = document.querySelector('.arenas')
+const $randomButton = document.querySelector('.button')
 const player1 = {
     name:'Scorpion',
-    hp: 30,
+    hp: 100,
     img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['blade','gun','fire'],
     attack: function(){
         console.log(this.name+'Fight..')
-    }
+    },
+    player:1,
 }
 const player2 = {
     name:'Sonya',
-    hp: 60,
+    hp: 100,
     img:'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['leg','knife','ball'],
     attack: function(){
         console.log(this.name+'Fight..')
-    }
+    },
+    player:2,
 }
-const createPlayer = function(classPlayer, {name, hp, img}){
-    const $player1 = document.createElement('div')
-    $player1.classList.add(classPlayer)
-    const $progressBar = document.createElement('div')
-    $progressBar.classList.add('progressbar')
-    const $life = document.createElement('div')
-    $life.classList.add('life')
-    $life.style.width = hp+'%'
-    const $namePlayer = document.createElement('div')
-    $namePlayer.classList.add('name')
-    $namePlayer.innerText = name
+const createElement = function(tag, className){
+    const $tag = document.createElement(tag);
+    if(className){
+    $tag.classList.add(className)}
+    return $tag
+}
+
+const createPlayer = function(playerObj){
+    const $player = createElement('div', 'player'+playerObj.player)
+    const $progressBar = createElement('div','progressbar')
+    const $life = createElement('div', 'life')
+    const $namePlayer = createElement('div', 'name')
+    const $character = createElement('div','character')
+    const $img1 =  createElement('img')
+
+   
+
+    $img1.src = playerObj.img     
+    $life.style.width = playerObj.hp+'%'      
+    $namePlayer.innerText = playerObj.name
+
     $progressBar.appendChild($life)
     $progressBar.appendChild($namePlayer)
-    const $character = document.createElement('div')
-    $character.classList.add('character')
-    const $img1 = document.createElement('img')
-    $img1.src = img
     $character.appendChild($img1)
-    $player1.appendChild($character)
-    $player1.appendChild($progressBar)
-    const $arenas = document.querySelector('.arenas')
-    $arenas.appendChild($player1)
+    $player.appendChild($character)
+    $player.appendChild($progressBar)
+    
+    return $player
 }
-createPlayer('player1', player1)
-createPlayer('player2', player2)
+
+
+function changeHp(player) {
+    const $playerLife = document.querySelector('.player' + player.player + ' .life');
+      
+    player.hp-=Math.ceil(Math.random()*20);
+    
+    $playerLife.style.width = player.hp+'%';
+    if (player.hp<=0){
+        player.hp=0
+        $playerLife.style.width = player.hp+'%';
+        if(player.player == 1 ? player=player2 : player=player1){
+        $arenas.appendChild(playerWin(player.name))}
+        $randomButton.disabled = true;
+    }
+}
+
+function playerWin(name) {
+    const $loseTitle = createElement('div', 'loseTitle')
+    $loseTitle.innerText = name + ' win'
+    return $loseTitle
+}
+
+$randomButton.addEventListener("click", function () {
+    changeHp(player1)
+    changeHp(player2)
+})
+
+$arenas.appendChild(createPlayer(player1))
+$arenas.appendChild(createPlayer(player2))
